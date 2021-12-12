964de63de699d5a5906bfac2486909f5de0a347d
Ostap Pokornyi <ostappokornyi@gmail.com>
2021-12-12 13:51:47 +0200

    last update

--- 01_git/scissors.c
+++ 01_git/scissors.c
@@ -6,46 +6,39 @@ const char arr[3] = {'r', 'p', 's'};
 
 int machine_rand(void) {
     srand(time(0));
-    int machine_chose = rand() % 3;
-    return machine_chose;
+    int random = rand() % 3;
+    return random;
 }
 
-// Rock vs Paper -> Paper wins.
-// Rock vs Scissor -> Rock wins.
-// Paper vs Scissor -> Scissor wins.
-
-// 0 - machine_wins
-// 1 - user_wins
-
-int start_game(char input_user, char *machine_choice){
-    int machine_chose  = machine_rand();
-    *machine_choice = arr[machine_chose];
+int start_game(char input_user, char *machine_choice_c){
+    int machine_choice_int  = machine_rand();
+    *machine_choice_c = arr[machine_choice_int];
     int result;
-    if(input_user == arr[machine_chose])
+    if(input_user == arr[machine_choice_int])
     {
         result = -1;
     }
-    else if (input_user == 'r' && arr[machine_chose] == 's')
+    else if (input_user == 'r' && arr[machine_choice_int] == 's')
     {
         result = 1;
     }
-    else if (arr[machine_chose] == 'r' && input_user == 's')
+    else if (arr[machine_choice_int] == 'r' && input_user == 's')
     {
         result = 0;
     }
-    else if (input_user == 'p' && arr[machine_chose] == 'r')
+    else if (input_user == 'p' && arr[machine_choice_int] == 'r')
     {
         result = 1;
     }
-    else if (arr[machine_chose] == 'p' && input_user == 'r')
+    else if (arr[machine_choice_int] == 'p' && input_user == 'r')
     {
         result = 0;
     }
-    else if (input_user == 's' && arr[machine_chose] == 'p')
+    else if (input_user == 's' && arr[machine_choice_int] == 'p')
     {
         result =  1;
     }
-    else if (arr[machine_chose] == 's' && input_user == 'p')
+    else if (arr[machine_choice_int] == 's' && input_user == 'p')
     {
         result =  0;
     }
@@ -55,22 +48,21 @@ int start_game(char input_user, char *machine_choice){
 
 
 int main () {
-    char input_from_user, machine_chose;
     int result;
+    char input_from_user, machine_chose;
     printf("Please choose: rock (r) - paper (p) - scissors (s)\n");
     scanf("%c", &input_from_user);
     result = start_game(input_from_user, &machine_chose);
-    // printf("THE machine_choice = %c", machine_chose);
     if(result == -1)
     {
-        printf("\nthe Friendship wins !! \n");
+         printf("\nYou chose %c and I choose %c, The Friendship wins ! \n\n", input_from_user, machine_chose);
     }
     else if (result == 0)
     {
-        printf("\nYou chose %c and I choose %c, HAHAHAH Machine wins \n\n", input_from_user, machine_chose);
+        printf("\nYou chose %c and I choose %c, HAHAHAH Machine wins ! \n\n", input_from_user, machine_chose);
     }
     else
     {
-        printf("\nYou chose %c and I choose %c, You wins \n\n", input_from_user, machine_chose);
+        printf("\nYou chose %c and I choose %c, You wins !\n\n", input_from_user, machine_chose);
     }
 }
